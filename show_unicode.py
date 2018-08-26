4adc7f88ffe65c9797d774b047e6e5a73f518711
Jeremy Fluhmann <fluhmann@gmail.com>
2018-08-26 16:41:48 -0500

    cleaned up garbage from my original copy/paste of show_unicode.py

--- show_unicode.py
+++ show_unicode.py
@@ -9,21 +9,6 @@ filename = sys.argv[1]
 with open(filename,'r', encoding='utf-8') as f:
     data=f.read()
 
-DEBUG=False
-#DEBUG=True
-# Replace some knowns
-# https://www.fileformat.info/info/unicode/block/general_punctuation/images.htm
-if not DEBUG:
-    data = data.replace(u"\u2019", "'").replace(u"\u2018", "'")           # Unicode Character 'RIGHT (U+2019) and LEFT (U+2018) SINGLE QUOTATION MARK'
-    data = data.replace(u"\u201d", "\"").replace(u"\u2013", "\"")         # Unicode Character 'RIGHT DOUBLE QUOTATION MARK' (U+201D)
-    #data = data.replace(u"replace(u"\u2013", "\"")                        # Unicode Character 'RIGHT DOUBLE QUOTATION MARK' (U+201D)
-    data = data.replace(u"\u00Bd", "1/2")                                 # Unicode Character 'VULGAR FRACTION ONE HALF' (U+00BD)
-#    data = data.replace(u"\u2013", "\"
-
-data = data.replace("\x0C","")               # Form Feed or New Paper sheet
-
-#print(data.encode('raw_unicode_escape'))
-
 unicode_match = re.compile(r'(\\u[\w]{4})', re.IGNORECASE)
 unicode_data = data.encode('raw_unicode_escape')
 
